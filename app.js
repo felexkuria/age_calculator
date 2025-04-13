3e3d312a41b53ee3c4165799b28eaeb64ce66df9
Felex Kuria <engineerfelex@gmail.com>
2025-04-13 08:39:45 +0300

    finished

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const date = document.getElementById('birthDate');
 const result = document.getElementById('ageOutput');
 const button = document.getElementById('calculateAge');
-button.addEventListener('click', calculateAge);
-function calculateAge() {
-    console.log(date.value);
-}
+button.addEventListener('click', () => {
+
+    let age = Math.floor((Date.now() - Date.parse(date.value)) / (1000 * 60 * 60 * 24 * 365.25));
+    if (isNaN(age)) {
+        console.log('Please enter a valid date');
+    } else {
+        console.log(age);
+        result.innerHTML = 'Your Age is ' + age + '  Years old';
+    }
+});
